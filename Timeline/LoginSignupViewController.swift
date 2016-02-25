ef6193c488427fcfc2f22f091c6bd073c91da27d
Pierre Toroyan <p.toroyan@gmail.com>
2016-02-25 00:11:08 -0700

    Signup/Login Class Implementation
    -authenticate users
    -create users

--- Timeline/LoginSignupViewController.swift
+++ Timeline/LoginSignupViewController.swift
@@ -10,15 +10,113 @@ import UIKit
 
 class LoginSignupViewController: UIViewController {
 
+    
+    enum ViewMode {
+        case Signup
+        case Login
+    }
+    
+    var viewMode = ViewMode.Signup
+    
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var bioField: UITextField!
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    
+    
+    
+    @IBAction func actionButtonTapped(sender: AnyObject) {
+        if fieldsAreValid {
+            switch viewMode {
+                case .Login:
+                    UserController.authenticateUser(emailField.text!, password: passwordField.text!, completion: { (success, user) -> Void in
+                        if success, let _ = user {
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        
+                        } else {
+                            self.presentValidationAlertWithTitle("Check fields", message: "please make sure everything is filled")
+                        }
+                    })
+            case .Signup:
+                    UserController.createUser(emailField.text!, username: usernameField.text!, bio: bioField.text, url: urlField.text, completion: { (success, user) -> Void in
+                        if success, let _ = user {
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        
+                        } else {
+                            self.presentValidationAlertWithTitle("Unable to Singup", message: "please try again")
+                        }
+                    })
+            }
+            
+        } else {
+            presentValidationAlertWithTitle("Uhm...", message: "please try again")
+        }
+        
+    }
+    
+    
+    func presentValidationAlertWithTitle(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    var fieldsAreValid: Bool {
+        get {
+            switch viewMode {
+            case .Login:
+                return !(emailField.text!.isEmpty || passwordField.text!.isEmpty)
+            case .Signup:
+                return !(usernameField.text!.isEmpty || emailField.text!.isEmpty || passwordField.text!.isEmpty)
+            }
+        }
+    }
+    
+    
+    func updateViewBasedOnMode() {
+        switch viewMode {
+        case .Signup:
+            actionButton.setTitle("Signup", forState: .Normal)
+        case .Login:
+            usernameField.hidden = true
+            bioField.hidden = true
+            urlField.hidden = true
+            
+            actionButton.setTitle("Login", forState: .Normal)
+        }
+    }
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateViewBasedOnMode()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
